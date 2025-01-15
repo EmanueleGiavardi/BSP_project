6c148b33c2a50a714363bcedc67733ed27f3e4d4
giava <emanuele.giavardi@gmail.com>
2025-01-15 10:01:04 +0000

    renaming & fixes

--- src/modules/Evaluator.py
+++ src/modules/Evaluator.py
@@ -1,17 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from modules import StochasticProcess
+
 
 class Evaluator:
-    def __init__(self, FECG_averages, gt_FECG_averages):
+    def __init__(self, S5, gt_S5):
         """
         Creates an Evaluator: an object that implements different methods to evaluate the estimated fetal complexes with respect to the truth ones
 
         Args: 
-            FECG_averages (dict): contains the estimated average fetal ECG complexe for each realization of the process
-            gt_FECG_averages (dict): contains the truth average fetal ECG complexe for each realization of the process
+            FECG_averages (StochasticProcess): contains the estimated average fetal ECG complexe for each realization of the process
+            gt_FECG_averages (StochasticProcess): contains the truth average fetal ECG complexe for each realization of the process
         """
-        self.FECG_averages = FECG_averages
-        self.gt_FECG_averages = gt_FECG_averages
+        self.S5 = S5
+        self.gt_S5 = gt_S5
 
 
     def get_correlations(self):
@@ -19,38 +21,32 @@ class Evaluator:
         Computes the correlation between the estimated average FECG and the estimated truth FECG, for each realization
 
         Returns:
-            correlations (dict): correlation values for each realization
+            correlations (array): correlation values for each realization
             mean_correlation (float): the mean correlation value (on the realizations)
         """
-        correlations = {}
-        mean_correlation = 0
+        correlations = []
 
-        for label in self.FECG_averages:
-            correlation = np.corrcoef(self.FECG_averages[label], self.gt_FECG_averages[label])[0, 1]
-            mean_correlation += correlation
-            correlations[label] = correlation
+        for i in range(self.S5.num_realizations):
+            correlation = np.corrcoef(self.S5.get_realization_by_index(i), self.gt_S5.get_realization_by_index(i))[0, 1]
+            correlations.append(correlation)
 
-        mean_correlation = mean_correlation / len(self.FECG_averages)
-        return correlations, mean_correlation
+        return correlations, np.mean(correlations)
 
     def get_MSEs(self):
         """
         Computes the Mean Squared Error between the estimated average FECG and the estimated truth FECG, for each realization
 
         Returns:
-            MSEs (dict): MSE values for each realization
+            MSEs (array): MSE values for each realization
             mean_MSE (float): the mean MSE value (on the realizations)
         """
-        MSEs = {}
-        mean_MSE = 0
+        MSEs = []
 
-        for label in self.FECG_averages:
-            MSE = np.mean((self.FECG_averages[label] - self.gt_FECG_averages[label])**2)
-            mean_MSE += MSE
-            MSEs[label] = MSE
-        
-        mean_MSE = mean_MSE / len(self.FECG_averages)
-        return MSEs, mean_MSE
+        for i in range(self.S5.num_realizations):
+            MSE = np.mean((self.S5.get_realization_by_index(i) - self.gt_S5.get_realization_by_index(i))**2)
+            MSEs.append(MSE)
+
+        return MSEs, np.mean(MSEs)
 
 
     def plot_comparison(self, correlations, MSEs):
@@ -58,15 +54,16 @@ class Evaluator:
         Plot the comparison between the average FCEG with estimated and ground truth peaks for each realization, with the associated metrics 
 
         Args:
-            correlations (dict): correlation values for each realization
-            MSEs (dict): MSE values for each realization
+            correlations (array): correlation values for each realization
+            MSEs (array): MSE values for each realization
         """
-        for label in self.FECG_averages:
-            print(f"\n--------------------------------{label}--------------------------------\n")
-            print(f"Correlation value for realization {label} = {correlations[label]}\n")
-            print(f"MSE per realizzazione {label} = {MSEs[label]}\n")
+        for i in range(self.S5.num_realizations):
+            label = f"AECG{i+1}"
+            print(f"\n----------------------------------------------------------------------------------------------------------------------\n")
+            print(f"Correlation value for realization {label} = {correlations[i]}\n")
+            print(f"MSE per realizzazione {label} = {MSEs[i]}\n")
             plt.figure(figsize=(12, 8))
-            plt.plot(self.FECG_averages[label], color='red', label=f"Average FECG - estimated peaks")
-            plt.plot(self.gt_FECG_averages[label], color='blue', label=f"Average FECG - ground truth peaks")
+            plt.plot(self.S5.get_realization_by_index(i), color='red', label=f"{label}: Average FECG - estimated peaks")
+            plt.plot(self.gt_S5.get_realization_by_index(i), color='blue', label=f"{label}: Average FECG - ground truth peaks")
             plt.legend()
             plt.show()
