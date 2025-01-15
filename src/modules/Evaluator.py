38ecc4e806d29eaf1f7fcdd4959d6464d58edfcf
Emanuele Giavardi <emanuele.giavardi@gmail.com>
2025-01-15 22:00:33 +0100

    graphical fixes

--- src/modules/Evaluator.py
+++ src/modules/Evaluator.py
@@ -59,11 +59,17 @@ class Evaluator:
         """
         for i in range(self.S5.num_realizations):
             label = f"AECG{i+1}"
-            print(f"\n----------------------------------------------------------------------------------------------------------------------\n")
-            print(f"Correlation value for realization {label} = {correlations[i]}\n")
-            print(f"MSE per realizzazione {label} = {MSEs[i]}\n")
             plt.figure(figsize=(12, 8))
             plt.plot(self.S5.get_realization_by_index(i), color='red', label=f"{label}: Average FECG - estimated peaks")
             plt.plot(self.gt_S5.get_realization_by_index(i), color='blue', label=f"{label}: Average FECG - ground truth peaks")
+
+            text_x = 0.05 * len(self.S5.get_realization_by_index(i))
+            text_y = max(self.S5.get_realization_by_index(i)) * 0.9
+            
+            metrics_text = (f"Correlation: {correlations[i]:.4f}\n"
+                            f"MSE: {MSEs[i]:.4f}")
+            
+            plt.text(text_x, text_y, metrics_text, fontsize=11, 
+                 bbox=dict(facecolor='white', alpha=0.8, edgecolor='black'))
             plt.legend()
             plt.show()
